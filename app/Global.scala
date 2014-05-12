6538cf175bd3c74118cf037733fd92810ebba897
Shunji Konishi <shun.konishi@nifty.com>
2014-05-12 21:15:51 +0900

    Add polling

--- app/Global.scala
+++ app/Global.scala
@@ -1,8 +1,23 @@
-import play.api.GlobalSettings
+import play.api._
+import play.api.libs.concurrent.Akka
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
 import play.api.mvc._
+import play.api.Play.current
+import play.api.libs.ws.WS
+import scala.concurrent.duration.DurationInt
 
 object Global extends GlobalSettings {
-  
+
+  override def onStart(app: Application) = {
+    sys.env.get("POLLING_URL").foreach { url =>
+      Akka.system.scheduler.schedule(0 seconds, 50 minutes) {
+        WS.url(url).get.map { response =>
+          Logger.info("polling: " + url + ", " + response.status)
+        }
+      }
+    }
+  } 
+
   override def onRouteRequest(req: RequestHeader): Option[Handler] = {
     if (req.method == "GET" &&
         req.headers.get("Upgrade").exists(_.equalsIgnoreCase("websocket"))) {
